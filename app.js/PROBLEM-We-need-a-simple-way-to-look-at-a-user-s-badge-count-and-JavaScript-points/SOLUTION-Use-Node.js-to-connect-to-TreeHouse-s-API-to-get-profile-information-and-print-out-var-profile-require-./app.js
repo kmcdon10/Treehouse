3a95afc9e7a8f0c933f2b90fa713dee9c931b399
Kate Mcdonald <katemcdonald39@gmail.com>
2015-07-20 13:47:13 -0500

    Delete app.js

--- app.js/PROBLEM-We-need-a-simple-way-to-look-at-a-user-s-badge-count-and-JavaScript-points/SOLUTION-Use-Node.js-to-connect-to-TreeHouse-s-API-to-get-profile-information-and-print-out-var-profile-require-./app.js
+++
@@ -1,7 +0,0 @@
-// PROBLEM: We need a simple way to look at a user's badge count and JavaScript points
-// SOLUTION: Use Node.js to connect to TreeHouse's API to get profile information and print out
-
-
-var profile = require("./profile.js");
-var users = process.argv.slice(2);
-users.forEach(profile.get);
